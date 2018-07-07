d218bb798141ca9220d3c118e277c5e56d24996f
Jens Beernaert <jens_beernaert@hotmail.com>
2018-07-07 18:32:57 +0200

    v0 - alhpa stage

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="container">
+        <div class="row text-center align-items-center">
+            <div class="col-md-2 mb-3">
+                <img src="http://pluspng.com/img-png/fifa-logo-png-fifa-logo-685.png" class="card-img-top">
+            </div>
+            <div class="col-md-8 mb-3">
+                <h1 class="display-4">Welcome</h1>
+                <p class="lead">Personal FIFA 19 manager</p>
+            </div>
+            <div class="col-md-2 mb-3">
+                <img src="https://s3.amazonaws.com/freebiesupply/large/2x/playstation-logo-png-transparent.png" alt=""
+                     class="card-img-top">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <ul class="list-group mb-3">
+                    <li class="list-group-item"><h1>Recent Updates</h1></li>
+                    <li class="list-group-item"><b>v0.*</b> - Alpha stage</li>
+                </ul>
+            </div>
+            <div class="col-md-6">
+                <ul class="list-group mb-3">
+                    <li class="list-group-item"><h1>Last scoring players</h1></li>
+                    <li class="list-group-item">Messi ♦ 5 goals</li>
+                    <li class="list-group-item">Christiano Ronaldo ♦ 5 goals</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+@endsection
+
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar sticky-top navbar-expand-md navbar-dark bg-dark" role="navigation">
+    <a class="navbar-brand" href="">FIFA 19</a>
+    <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+            <li class="nav-item"><a class="nav-link" href="">Link</a></li>
+        </ul>
+        <ul class="nav navbar-nav ml-auto">
+            <!-- RIGHT FLOATING LINKDS -->
+        </ul>
+    </div>
+</nav>
